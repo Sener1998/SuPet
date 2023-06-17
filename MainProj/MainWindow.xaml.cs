f5a512cd1c83e9b7b7a0aa079eaf61e60936f4ee
Sener <liuqinchao1998@163.com>
2023-06-17 21:22:19 +0800

    Add features: 1. Hide the window 2. Load the image 3. Drag the image

--- MainProj/MainWindow.xaml.cs
+++ MainProj/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfAnimatedGif;
 
 namespace MainProj
 {
@@ -23,6 +26,33 @@ namespace MainProj
         public MainWindow()
         {
             InitializeComponent();
+
+            // 设置窗口
+            this.AllowsTransparency = true;
+            this.WindowStyle = WindowStyle.None;
+            this.ResizeMode = ResizeMode.NoResize;
+
+            // 加载Gif
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.UriSource = new Uri(@"D:\Projects\Temp\happy.gif");
+            bitmapImage.EndInit();
+            ImageBehavior.SetAnimatedSource(myImage, bitmapImage);
         }
+
+        private void myImage_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void myImage_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Trace.WriteLine("Hello!");
+            //MessageBox.Show("Hello!!!");
+        }
+
     }
 }
